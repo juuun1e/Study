06a2cc3123d200b49e4f353ea17df6bbb496695a
juuun1e <wldn5959@naver.com>
2024-01-29 23:31:44 +0900

    ch05.객체와클래스_사용자정의자료형 만들고 사용하기

---
+++ IntellJJava/src/com/example/part2/ch05/BookTest.java
@@ -0,0 +1,24 @@
+package com.example.part2.ch05;
+
+public class BookTest {
+  public static void main(String[] args) {
+    //Q. [정수] 한 개를 저장하는 [변수를 선언]하시오.
+    int a;
+
+    //Q. 한 권의 책 데이터를 저장하고출력하시오.(6개 변수)
+    //변수 개별적, 한 권의 책이라고 볼 수 있는가
+    String title="java";
+    int price=28000;
+    String company="fastcampus";
+    String author="박자바";
+    int page=687;
+    String isbn="ISBN-111990011";
+    System.out.println(title+"\t"+price+"\t"+company+"\t"+author+"\t"+page+"\t"+isbn);
+
+    //Q. 6개의 변수를 하나의 구조에 담기 위해 Book이라는 바구니(이동)을 만들어보시오.
+    //Q. [책] 한 권을 저장하는 [변수를 선언]하시오. --> 기본 자료형이 없으니까 만들어야 함
+    //Book b;
+
+
+  }
+}
