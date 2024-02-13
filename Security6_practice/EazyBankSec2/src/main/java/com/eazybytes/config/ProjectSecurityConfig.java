b7d12266a74982e3f25e2187de211955a7675d66
juuun1e <wldn5959@naver.com>
2024-02-14 03:57:59 +0900

    JdbcUserDetailsManager vs 사용자 정의 테이블 생성

--- Security6_practice/EazyBankSec2/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ Security6_practice/EazyBankSec2/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -4,12 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -53,7 +54,7 @@ public class ProjectSecurityConfig {
     //userDetailsManager Bean을 생성하고 구축할 때마다
     //DaoAuthenticationProvider는 InMemory 안에 있는 메소드를 호출하여 사용자 인증하려고 함
     //→ 호출한 메서드 : loadUserByUsername , 리턴타입(UserDetails)
-    @Bean
+/*    @Bean
     public InMemoryUserDetailsManager userDetailsService() {
 
         //username을 직접 사용하지 않고 .withUsername메소드 호출
@@ -67,7 +68,17 @@ public class ProjectSecurityConfig {
             .build();
         return new InMemoryUserDetailsManager(admin, user);
         //InMemoryUserDetailsManager의 도움으로 UserDetails구성
+    } */
+    //-> 더 이상 사용 안함. Jdbc방식의 인증 사용
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+        // DataSouce객체가 JdbcUserDetailsManager에게 넘어갈 때마다 스프링부트는 데이터 소스 객체를 자동적으로 웹 애플리케이션 내부에 생성,
+        // JdbcUserDetailsManager에게 데이터베이스를 생성하였으며 그에 대한 정보는 이  DataSource 에 있습니다~ 하고 알려주는 것
+        // : Manager 객체 생성의 이유
     }
+
 
     // 정상적 수행 = 반드시 웹 어플리케이션 안에서 비밀번호와 할당량 유형의 Bean을 생성해야 함
     // 그러므로 동일한 작업을 위해 PasswordEncoder유형의 Bean을 반환할 새로운 메소드 생성
