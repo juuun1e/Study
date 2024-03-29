45b7ce16a6f2719bd66478fa4ab326a0d32a9750
juuun1e <wldn5959@naver.com>
2024-03-29 23:31:38 +0900

    Calculator 이자계산기

--- IntellJJava/src/com/codewithmosh/MortgageCalculator.java
+++ IntellJJava/src/com/codewithmosh/MortgageCalculator.java
@@ -8,26 +8,46 @@ public class MortgageCalculator {
     final byte MONTHS_IN_YEAR = 12; // 12개월 설정
     final byte percent = 100; // 백분율 설정
 
+    int principal = 0;
+    float monthlyInterest = 0;
+    int numberOfPayments = 0;
+
     Scanner sc = new Scanner(System.in);
 
-    System.out.print("Princical: ");
-    int principal = sc.nextInt();
+    while (true) {
+      System.out.print("Princical ($1k - $1M): ");
+      principal = sc.nextInt();
+      if (principal >= 1000 && principal <= 1_000_000)
+        break;
+      System.out.println("Enter a number between 1,000 and 1,000,000.");
+    }
 
-    System.out.print("Annual Interest Rate: ");
-    float annualInterest = sc.nextFloat(); // 이자율은 작은 숫자이므로 double 대신 float 사용
-    float monthlyInterest = annualInterest / percent / MONTHS_IN_YEAR;
-    // 숫자로만 넣으면 다른 사람이 이해하기 힘들기 때문에 상단에 상수로 정해 선언
-    // m1 m2 와 같은 변수 이름은 지양. 항상 의미있고 설명이 포함된 이름을 사용하기
+    while (true) {
+      System.out.print("Annual Interest Rate: ");
+      float annualInterest = sc.nextFloat(); // 이자율은 작은 숫자이므로 double 대신 float 사용
+      if (annualInterest >= 1 && annualInterest <= 30) {
+        monthlyInterest = annualInterest / percent / MONTHS_IN_YEAR;
+        break;
+      }
+      System.out.println("Enter a value between 1 and 30.");
+    }
 
-    System.out.print("Period(Years): ");
-    byte years = sc.nextByte();
-    int numberOfPayments = years * MONTHS_IN_YEAR;
+    while (true) {
+      System.out.print("Period(Years): ");
+      byte years = sc.nextByte();
+      if (years >= 1 && years <= 30) {
+        numberOfPayments = years * MONTHS_IN_YEAR;
+        break;
+      }
+      System.out.println("Enter a value between 1 and 30.");
+    }
 
     double mortgage = principal
-                * (monthlyInterest * Math.pow(1+monthlyInterest,numberOfPayments))
-                / (Math.pow(1+monthlyInterest,numberOfPayments)-1);
+        * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+        / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
 
     String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
     System.out.print("Mortgage: " + mortgageFormatted);
   }
+
 }
