3d625c1875f6aca7fa8502e4b9b240fd45a9e088
juuun1e <wldn5959@naver.com>
2024-04-05 00:37:53 +0900

    스프링 웹개발 기초(xml/MVC+template/API)

--- SpringBoot/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ SpringBoot/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,5 +13,38 @@ public class HelloController {
   public String hello(Model model){
     model.addAttribute("data","hello!!");
     return "hello";
+  }
+
+  @GetMapping("hello-mvc")
+  public String helloMvc(@RequestParam("name") String name, Model model){
+    model.addAttribute("name",name);
+    return "hello-template";
+  }
+
+    @GetMapping("hello-string")
+    @ResponseBody // http의 body부분에 이 데이터를 직접 넣어주겠다는 의미
+    public String helloString(@RequestParam("name") String name) {
+      return "hello " + name; // view 없이 문자가 그대로 들어감
+    }
+
+  @GetMapping("hello-api")
+  @ResponseBody
+  public Hello helloApi(@RequestParam("name") String name) {
+    Hello hello = new Hello();
+    hello.setName(name);
+    return hello;
+  }
+  // static class : 인스턴스(객체)생성 없이 바로 사용 가능
+  // -> HelloController class 에서 Hello static class를 그냥 사용할 수 있음
+  // -> HelloController.Hello
+  static class Hello {
+    private String name;
+    public String getName() {
+      return name;
+    }
+
+    public void setName(String name) {
+      this.name = name;
+    }
   }
 }
