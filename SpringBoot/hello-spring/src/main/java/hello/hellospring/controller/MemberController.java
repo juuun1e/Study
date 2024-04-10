f68543f1f72e1fb7cbd33a457bc646a62cd58e8f
juuun1e <wldn5959@naver.com>
2024-04-10 13:22:56 +0900

    직접 설정을 통한 스프링 빈 등록

--- SpringBoot/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ SpringBoot/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Controller;
 public class MemberController {
   private final MemberService memberService;
 
-  @Autowired //연관된 객체를 스프링컨테이너에서 찾아 넣어준다. => 의존성 주입 DI
+  @Autowired
   public MemberController(MemberService memberService) {
     this.memberService = memberService;
   }
