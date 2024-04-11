ba82f4ebeafc148a5b42176eabe1f2d1f60b68d1
juuun1e <wldn5959@naver.com>
2024-04-11 16:20:49 +0900

    회원관리 웹MVC개발 : 회원 조회 기능

--- SpringBoot/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ SpringBoot/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -15,6 +18,7 @@ public class MemberController {
   public MemberController(MemberService memberService) {
     this.memberService = memberService;
   }
+
   @GetMapping(value = "/members/new")
   public String createForm() {
     return "members/createMemberForm";
@@ -24,7 +28,16 @@ public class MemberController {
   public String create(MemberForm form) {
     Member member = new Member();
     member.setName(form.getName());
+
     memberService.join(member);
+
     return "redirect:/";
+  }
+
+  @GetMapping(value = "/members")
+  public String list(Model model) {
+    List<Member> members = memberService.findMembers(); // 모든 회원 목록 조회
+    model.addAttribute("members", members); // 회원 리스트 자체를 모델에 담아 화면에 넘김
+    return "members/memberList";
   }
 }
